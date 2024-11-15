f68ff2e313ab8b67135f7c790354ae9345c16c07
Kyle Essenmacher <kessenmacher7832@gmail.com>
2024-11-14 23:17:19 -0500

    testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,15 @@
-'use client'
-
+import React from 'react'
 import dynamic from 'next/dynamic'
-import { LoadingSpinner } from '../src/components/LoadingSpinner'
 
 const AppBuilderWrapper = dynamic(
     () => import('../src/components/AppBuilderWrapper'),
     {
         ssr: false,
-        loading: () => <LoadingSpinner />
+        loading: () => <div>Loading Med-Flow...</div>
     }
 )
 
-export default function Page() {
+export default function Home() {
     return (
         <div className="container mx-auto px-4 py-8">
             <h1 className="text-4xl font-bold mb-8">Welcome to Med-Flow</h1>
